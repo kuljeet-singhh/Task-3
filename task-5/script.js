bf600079b5f4828fcdd0edf7dfbca6233421491e
kuljeet-singhh <Kuljeet.pixlerlab@gmail.com>
2026-02-09 12:18:49 +0530

    update task-5

--- task-5/script.js
+++ task-5/script.js
@@ -16,6 +16,22 @@ for (let i = 0; i < array.length; i++) {
   console.log(array[i][col]);
 }
 
+
+// 44. Convert binary string to number
+// Hint: Treat string as array of characters and calculate value.
+// Array Example:
+// "1011" → ['1','0','1','1'] → 11
+
+console.log(" 44. Convert binary string to number")
+function binaryToNo(B){
+  return parseInt(B,2)
+}
+let binary = '1011'
+console.log("Binary = "+binary)
+let result5=binaryToNo(binary)
+console.log("No is = "+result5)
+
+
 // 45. Sum of numbers in a jagged array
 // Hint: If element is array, go inside it; if number, add it.
 // Array Example:
@@ -59,6 +75,36 @@ let data3 = [1, [2, [3]]];
 let flatData3 = data3.flat(Infinity);
 console.log("jagged array = " + flatData3);
 console.log(flatData3);
+
+
+// 48. Find longest word in a string
+// Hint: Split string into array and compare word lengths.
+// Array Example:
+// "I love JavaScript" → ["I","love","JavaScript"]
+
+
+console.log(" 48. Find longest word in a string")
+
+function largeW(s){
+let max=""
+  let result=s.split(" ");
+  // console.log(result)
+    for(let i=0;i<result.length;i++){
+    // console.log( result[i].length);
+     if( result[i].length>max.length){
+      max=result[i]
+     }
+    }
+
+console.log("longest word in a string = "+max)
+}
+
+let s="I love JavaScript";
+console.log(s)
+largeW(s);
+
+
+
 
 /* 
 49. Shuffle an array of strings
@@ -109,6 +155,27 @@ console.log(result1)
 
 
 
+// 51. Letter frequency in a string
+// Hint: Count letters and store as array pairs.
+// Array Example:
+// "hello" → [["h",1],["e",1],["l",2],["o",1]]
+console.log(" 51. Letter frequency in a string")
+function frequency(str){
+  let result=[];
+  for(let char of str){
+    if(char!==" "){
+      result[char]=(result[char]|| 0)+1;
+    }
+  }
+  return result;
+}
+
+let Letter="hello"
+console.log("string is : "+Letter)
+let result6=frequency(Letter)
+console.log(result6)
+
+
 /* 
 52. Fibonacci(500)
 Hint: Use BigInt to store large numbers.
@@ -140,3 +207,16 @@ Hint: Multiply numbers from 1 to 70 using BigInt.
 Array Example:
 70 × 69 × 68 × ... × 1
 */
+
+console.log("53. Calculate 70!")
+function multiply(n){
+let ans=1n;
+  for(let i=2n;i<=n;i++){
+     ans*=i;
+  }
+  return ans;
+};
+ let N =70;
+ let result4=multiply(N);
+
+ console.log("Factorial of 70! = "+result4)
